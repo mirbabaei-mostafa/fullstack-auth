a6fad32f490b4b85124562890db84b942d36a365
mostafa <mirbabaei.mostafa@gmail.com>
2023-12-19 15:53:24 -0500

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
