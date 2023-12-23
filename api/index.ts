efdb61d2ee24a0eb110413213c58570907602d57
mirbabaei-mostafa <mirbabaei.mostafa@gmail.com>
2023-12-23 15:31:20 +0330

    Create model and controller

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response, Application } from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routers/user.router";
+import express, { Express, Request, Response, Application } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routers/user.router';
 
 //For env File
 dotenv.config();
@@ -9,16 +9,16 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGOINFOLOCAL as string)
   .then(() => {
-    console.log("Connected");
+    console.log('Connected');
   })
   .catch((e) => {
     console.log(e.message);
   });
 const app: Application = express();
-const port: string = process.env.PORT || "8000";
+const port: string = process.env.PORT || '8000';
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
 
-app.use("/api/users", userRouter);
+app.use('/api/users', userRouter);
